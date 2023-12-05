9988bc14faa55a715f522fd27d2d99f7720f27cf
Guilherme Orugian <orugian@outlook.com>
2023-12-05 14:09:31 -0300

    commit para mandar para revisão

--- main.js
+++ main.js
@@ -3,22 +3,23 @@ $(document).ready(function () {
 
     e.preventDefault();
     const tarefa = $('form input').val();
-    const novaTarefa =  $('<li></li>');          //$('<li></li>'); // Prblema pode estar aqui!!!!!!
-    $(tarefa).appendTo(novaTarefa);
+    const novaTarefa =  $('li');          //$('<li></li>'); // Prblema pode estar aqui!!!!!!
+    
     $(`
-    <div class="tarefa">
+    <div class="lista">
     <a>
       ${tarefa}
     </a>
     </div>
     `).appendTo(novaTarefa);
+    $(tarefa).appendTo(novaTarefa);
     $(novaTarefa).appendTo('ul');
     $(novaTarefa).fadeIn(1000);
     $('#tarefa-nova').val(' ');
   })
   
-  $(novaTarefa.val()).click(function(a) {
-    // Adiciona a classe "clicked" ao link
+  $(novaTarefa).click(function(a) {
+  
     a.preventDefault();
     $(this).addClass('clicked');
   })
